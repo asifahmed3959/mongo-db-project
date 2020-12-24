c71c4e8f91232033c36010fe8ce5e26442ca405f
Asif Ahmed <asif.ahmed.apploye@gmail.com>
2020-12-24 19:24:53 +0600

    commit

--- data-generate.py
+++ data-generate.py
@@ -1,20 +1,34 @@
 import time
+import phonenumbers
 
 from pymongo import MongoClient
 from faker import Factory
+from faker.providers.phone_number.en_US import Provider
 
 #
 # client = MongoClient('mongodb://user:pass@host.domain.com:23325/db')
 # db = client.db
 
+class CustomPhoneProvider(Provider):
+    def phone_number(self):
+        while True:
+            phone_number = self.numerify(self.random_element(self.formats))
+            parsed_number = phonenumbers.parse(phone_number, 'US')
+            if phonenumbers.is_valid_number(parsed_number):
+                return phonenumbers.format_number(
+                    parsed_number,
+                    phonenumbers.PhoneNumberFormat.E164
+                )
+
 
 def create_data(fake):
-    """creates 10 fake data"""
-
+    """creates 10 fake data phone number of region us"""
+    fake.add_provider(CustomPhoneProvider)
     for x in range(10):
         generated_name = fake.first_name()
         generated_surname = fake.last_name()
         phone_number = fake.phone_number()
+        address = fake.address()
         # balance = fake
 
         # result = db.col1.insert_one(
@@ -28,11 +42,12 @@ def create_data(fake):
 
         d = {'name': generated_name,
              'surname': generated_surname,
-             'job': phone_number}
+             'phone_number': phone_number,
+             'address': address}
 
         print (d)
         # time.sleep(1)
 
 if __name__ == '__main__':
-    fake = Factory.create()
+    fake = Factory.create('en_US')
     create_data(fake)
